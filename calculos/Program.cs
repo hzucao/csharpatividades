6113ae588f11c140506131cc6b4dcb10e84a3808
Heitor Ilario Zucao <heitor.zucao@sp.senai.br>
2023-08-03 16:28:06 -0300

    aula 03/08

---
+++ calculos/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program{
+
+    public static void Main(){
+
+        Console.WriteLine("Digite um aluno");
+        string aluno = Console.ReadLine();
+        Console.WriteLine("Digite nota1");
+        float nota1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Digite nota2");
+        float nota2 = float.Parse(Console.ReadLine());
+
+        float media = (nota1+nota2)/2;
+        
+        Console.WriteLine("Aluno "+ aluno+ "tem a media:" + media);
+
+
+    }
+
+}
---
+++ media/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    public static void Main(){
+        Console.WriteLine("Digite a media");
+        float media = float.Parse(Console.ReadLine());
+        
+        if(media > 7){
+            Console.WriteLine("Aprovado");
+        }
+        else{
+            Console.WriteLine("Reprovado");
+        }
+
+    }
+}
