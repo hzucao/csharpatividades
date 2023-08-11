b6c10692140480cd69634d685e03115f0c164b63
Heitor Ilario Zucao <heitor.zucao@sp.senai.br>
2023-08-10 22:07:00 -0300

    Aula 10/08/2023 while dowhile e metodos

---
+++ dowhile/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Progam{
+
+    public static void Main(){
+        bool controle = false;
+
+        do{ // executa uma vez e depois entra no processo de validação do enquanto
+            Console.WriteLine("Digite um numero");
+            int numero = int.Parse(Console.ReadLine());
+            //chamada do metodo multiplicaDez dentro do WriteLine com a passagem da variavel numero como argumento.
+            Console.WriteLine(MultiplicaDez(numero));
+            Console.WriteLine("Deseja Multiplar outro numero? S-Sim N-Não");
+            string simNao = Console.ReadLine();
+
+            if(simNao == "S" || simNao == "s"){
+                controle = true;
+            }
+            else{
+                controle = false;
+            }
+        }while(controle); // controle do loop, "enquanto" a validação do while for verdade o loop vai continuar acontecendo.
+
+    }
+
+    public static int MultiplicaDez(int num ) //assinatura do metodo num é um parametro
+    {
+        return num*10; //metodo com retorno ja fazendo o calculo
+    }
+}
---
+++ funcwhile/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    public static void Main(){
+        Console.WriteLine("Digite um numero inteiro:");
+        int numero = int.Parse(Console.ReadLine());
+        
+        int controle = numero;
+
+        while(controle>0){ //Estura de while, enquanto a condição do while for verdadeira o codigo do laço sera executado. Obs. ele só executa desde a primeira vez se a condição for valida.
+            if(ePar(controle)){
+            Console.WriteLine(controle);
+            } 
+
+            controle = controle-1;
+
+        }
+
+        
+
+    }
+
+    public static bool ePar(int num){
+
+        if(num%2==0){
+            return true;
+            
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    public static bool ePar(int num, int num1){
+             return true;
+    }
+
+}
